55e88e984a5eb897be88e6660b3f92d5bd662a20
Jhon Moofarry <jhon.moofarry00@gmail.com>
2024-05-14 21:47:09 -0500

    finish player

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import svelte from '@astrojs/svelte';
 import react from '@astrojs/react';
 
-import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()],
+  integrations: [tailwind(), svelte(), react()],
   output: 'server',
   adapter: vercel()
 });
--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,22 +1,16 @@
-
-
 <script lang="ts">
-  
-  const currentTime= new Date()
+  const currentTime = new Date()
   const currentHour = currentTime.getHours()
-  let greeting = ''
+
+  let greeting = ""
 
   if (currentHour < 12) {
-    greeting = 'Buenos días'
+    greeting = "Buenos días"
   } else if (currentHour < 18) {
-    greeting = 'Buenas tardes'
+    greeting = "Buenas tardes"
   } else {
-    greeting = 'Buenas noches'
+    greeting = "Buenas noches"
   }
-
-
 </script>
 
-<h1 class="text-3xl font-bold">
-  {greeting}
-</h1>
+<h1 class="text-3xl font-bold">{greeting}</h1>
